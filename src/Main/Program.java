2a4eb6d83d622ba661d51a8bf4e96095736ea08e
UnGuZartex <cobra0415@hotmail.com>
2020-02-20 12:04:43 +0100

    FIRST LINE!

---
+++ src/Main/Program.java
@@ -0,0 +1,7 @@
+package Main;
+
+public class Program {
+    public static void main(String args[]){
+        System.out.println("Kip");
+    }
+}
---
+++ src/Tests/ProgramTest.java
@@ -0,0 +1,7 @@
+package Tests;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProgramTest {
+
+}
