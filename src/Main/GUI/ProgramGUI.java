876b655451021a3184c59c17a8a83e66f0859c5c
Louis Carpentier <louis.carpentier.business@gmail.com>
2020-03-01 14:46:06 +0100

    Minor update to grid

--- src/Main/GUI/ProgramGUI.java
+++ src/Main/GUI/ProgramGUI.java
@@ -11,10 +11,16 @@ public class ProgramGUI {
     public static void main(String args[]){
         BlockrCanvas canvas = new BlockrCanvas("Nice");
 
+        int[][] list = new int[2][5];
+
+
+        System.out.println(list.length);
+        System.out.println(list[0].length);
+
         canvas.setDimensions(800, 500);
 
         java.awt.EventQueue.invokeLater(() -> {
-          canvas.show();
+          //canvas.show();
         });
     }
 }
