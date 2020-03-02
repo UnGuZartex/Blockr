f7ace51cc5743eaa917b5e4f91853aa59c7e9ff6
Louis Carpentier <louis.carpentier.business@gmail.com>
2020-03-02 15:05:18 +0100

    Start implementation of block structure

--- src/Main/BlockStructure/Connections/Plug.java
+++ src/Main/BlockStructure/Connections/Plug.java
@@ -1,5 +1,7 @@
 package Main.BlockStructure.Connections;
 
+
+
 public class Plug extends Connector {
 
 
@@ -9,37 +11,49 @@ public class Plug extends Connector {
         super(orientation);
     }
 
+
+    // Basic inspector
     public Socket getSocket() {
         return socket;
     }
 
-
-
-
-
+    /**
+     * Check whether this plug is connected to any socket.
+     *
+     * @return True if and only if this plug has an existing socket.
+     */
     public boolean isConnected() {
         return socket != null;
     }
 
     /**
-     * Check whether this plug can have the given socket as its socket.
+     * Checks whether this plug has a proper socket.
+     *
+     * @return True if and only the socket of this plug is not existing
+     *         or the plug connected to the socket of this plug equals
+     *         this plug.
+     */
+    public boolean hasProperSocket() {
+        return socket == null || socket.getPlug() == this;
+    }
+
+    /**
+     * Checks whether this plug can have the given socket as its socket.
      *
      * @param    socket
      *           The socket to check.
      *
-     * @return   True if and only if the given socket is not null.
+     * @return   True if and only if the given socket is effective.
      */
     public boolean canHaveAsSocket(Socket socket) {
         return socket != null;
     }
 
-    /**
-     * Check whether this plug has a proper socket attached to it.
-     * @return
-     */
-    public boolean hasProperSocket() {
-        return canHaveAsSocket(socket) && ( socket == null || socket.getPlug() == this);
-    }
+
+
+
+
+
 
     public void connect (Socket socket) throws Exception {
         if (!canHaveAsSocket(socket)) {
@@ -49,19 +63,12 @@ public class Plug extends Connector {
             throw new Exception();
         }
         if (isConnected()) {
-            this.socket.disconnect();
+            disconnect();
         }
-        setSocket(socket);
+        this.socket = socket;
         socket.connect(this);
     }
 
-
-    private void setSocket(Socket socket) throws Exception{
-        if (!canHaveAsSocket(socket)) {
-            throw new Exception();
-        }
-        this.socket = socket;
-    }
 
 
 
---
+++ src/Main/BlockStructure/Functionality/Operation.java
@@ -0,0 +1,9 @@
+package Main.BlockStructure.Functionality;
+
+public abstract class Operation implements Condition {
+
+
+
+
+
+}
