ab8ea09390a4cec9367efedd54342ca3383d4044
UnGuZartex <cobra0415@hotmail.com>
2020-03-03 09:06:50 +0100

    Variable height

--- src/Main/GUI/ProgramGUI.java
+++ src/Main/GUI/ProgramGUI.java
@@ -10,7 +10,8 @@ import java.awt.*;
 public class ProgramGUI {
     public static void main(String args[]){
         BlockrCanvas canvas = new BlockrCanvas("Blockr");
-        canvas.setDimensions(1920, 1080);
+        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+        canvas.setDimensions(screenSize.width, screenSize.height);
 
         java.awt.EventQueue.invokeLater(() -> {
           canvas.show();
