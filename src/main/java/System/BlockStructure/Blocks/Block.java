7b1695b48747b227059321c735badbb1c63ca330
UnGu Zartex <yentl.kinoo@student.kuleuven.be>
2020-03-07 12:41:07 +0100

    Solved merge

--- src/main/java/System/BlockStructure/Blocks/Block.java
+++ src/main/java/System/BlockStructure/Blocks/Block.java
@@ -1,11 +1,6 @@
 package System.BlockStructure.Blocks;
-
-<<<<<<< HEAD:src/main/java/BlockStructure/Blocks/Block.java
-
-import BlockStructure.Functionality.Functionality;
-=======
 import System.BlockStructure.Functionality.IFunctionality;
->>>>>>> 8c3ed1b8bf3fdb92c75eaf46bcd0cac655e2609f:src/main/java/System/BlockStructure/Blocks/Block.java
+
 
 public abstract class Block<F extends IFunctionality> {
 
