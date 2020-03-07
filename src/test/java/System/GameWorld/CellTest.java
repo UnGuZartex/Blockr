d59dc8f4cbc28817dec978eaaaf73bd537e47360
Louis Carpentier <louis.carpentier.business@gmail.com>
2020-03-07 18:26:58 +0100

    Added aditional tests

--- src/test/java/System/GameWorld/CellTest.java
+++ src/test/java/System/GameWorld/CellTest.java
@@ -1,20 +1,32 @@
 package System.GameWorld;
 
+import org.junit.Assert;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class CellTest {
 
+    Cell cellBlank, cellWall, cellGoal;
+
     @BeforeEach
     void setUp() {
+        cellBlank = new Cell(CellType.BLANK);
+        cellWall =  new Cell(CellType.WALL);
+        cellGoal = new Cell(CellType.GOAL);
     }
 
     @AfterEach
     void tearDown() {
+        cellBlank = null;
+        cellWall = null;
+        cellGoal = null;
     }
 
     @Test
     void getCellType() {
+        Assert.assertEquals(CellType.WALL, cellWall.getCellType());
+        Assert.assertEquals(CellType.BLANK, cellBlank.getCellType());
+        Assert.assertEquals(CellType.GOAL, cellGoal.getCellType());
     }
 }
--- src/test/java/System/GameWorld/CellTypeTest.java
+++ src/test/java/System/GameWorld/CellTypeTest.java
@@ -1,24 +1,39 @@
 package System.GameWorld;
 
+import org.junit.Assert;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class CellTypeTest {
 
+    private CellType blank, wall, goal;
+
     @BeforeEach
     void setUp() {
+        blank = CellType.BLANK;
+        wall = CellType.WALL;
+        goal = CellType.GOAL;
     }
 
     @AfterEach
     void tearDown() {
+        blank = null;
+        wall = null;
+        goal = null;
     }
 
     @Test
     void canWalkOn() {
+        Assert.assertTrue(blank.canWalkOn());
+        Assert.assertFalse(wall.canWalkOn());
+        Assert.assertTrue(goal.canWalkOn());
     }
 
     @Test
     void isWin() {
+        Assert.assertFalse(blank.isWin());
+        Assert.assertFalse(wall.isWin());
+        Assert.assertTrue(goal.isWin());
     }
 }
