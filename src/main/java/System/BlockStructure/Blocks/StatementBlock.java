377e61d9ba7c152cb7e6e6a25e6b1fbefa308c6a
UnGuZartex <cobra0415@hotmail.com>
2020-03-10 11:01:10 +0100

    fixed operational

--- src/main/java/System/BlockStructure/Blocks/StatementBlock.java
+++ src/main/java/System/BlockStructure/Blocks/StatementBlock.java
@@ -1,15 +1,12 @@
 package System.BlockStructure.Blocks;
 
 
-import System.BlockStructure.Connectors.SubConnector;
 import System.BlockStructure.Functionality.ConditionalBlockFunctionality;
 
 public class StatementBlock extends ConditionalBlock {
 
-    private final SubConnector[] subConnectors;
     protected <B extends ConditionalBlock> StatementBlock( ConditionalBlockFunctionality<B> functionality) {
         super(functionality);
-        subConnectors = new SubConnector[] {};
 
     }
 
