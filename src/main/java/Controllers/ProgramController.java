5e643c94b11c0c6e20e0f0aff067e5e9c4cbe291
UnGuZartex <cobra0415@hotmail.com>
2020-03-12 12:13:13 +0100

    Solved merge

--- src/main/java/Controllers/ProgramController.java
+++ src/main/java/Controllers/ProgramController.java
@@ -1,28 +1,25 @@
 package Controllers;
 
-import GUI.GUIBlock;
-import GUI.GUISocket;
+
 import System.BlockStructure.Blocks.Block;
 import System.Logic.Converter;
 import System.Logic.ProgramArea.PABlockHandler;
 import System.Logic.ProgramArea.Program;
-import System.Logic.ProgramArea.ProgramArea;
 
-import java.util.List;
 
 public class ProgramController {
     private final PABlockHandler blockHandler = new PABlockHandler();
     private final Converter converter = new Converter();
 
-    public void addBlockToPA(GUIBlock block) {
-        Block toAdd = converter.convert(block.getID());
-        blockHandler.addToPA(toAdd);
-    }
-
-    public void connectToSubConnector(GUIBlock block, GUISocket socket) {
-        Block toAdd = converter.convert(block.getID());
-        blockHandler.addToPA(toAdd);
-    }
+//    public void addBlockToPA(GUIBlock block) {
+//        Block toAdd = converter.convert(block.getID());
+//        blockHandler.addToPA(toAdd);
+//    }
+//
+//    public void connectToSubConnector(GUIBlock block, GUIConnector connector) {
+//        Block toAdd = converter.convert(block.getID());
+//        blockHandler.addToPA(toAdd);
+//    }
 
     //TODO REMOVE V
     public void addBlock(Block block){
