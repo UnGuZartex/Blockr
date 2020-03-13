db15222b3035d8f98101f8aa7db15819d41bd2b2
UnGuZartex <cobra0415@hotmail.com>
2020-03-13 15:03:23 +0100

    Started

--- src/main/java/Controllers/GUItoSystemInterface.java
+++ src/main/java/Controllers/GUItoSystemInterface.java
@@ -14,6 +14,7 @@ public class GUItoSystemInterface {
     private final HashMap<GUIBlock, Block> conversionTable = new HashMap<>();
 
 
+
     public GUItoSystemInterface(PABlockHandler blockHandler) {
         this.blockHandler = blockHandler;
         factories.put("IF", new IfGUIFactory());
