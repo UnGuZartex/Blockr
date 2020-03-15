a14344ed8bfa2a1b39bc493d0669df7d5cedd5db
Louis Carpentier <louis.carpentier.business@gmail.com>
2020-03-15 15:55:13 +0100

    Documentaion on panel classes

--- src/main/java/GUI/Panel/GameWorldPanel.java
+++ src/main/java/GUI/Panel/GameWorldPanel.java
@@ -6,21 +6,59 @@ import GUI.Components.GUIGrid;
 
 import java.awt.*;
 
+/**
+ * A class for a game world panel, which can listen to the program.
+ *
+ * @author Alpha-team
+ */
 public class GameWorldPanel extends GamePanel implements ProgramListener {
 
+    /**
+     * Variable referring to the grid.
+     */
     private GUIGrid GUIGrid;
+    /**
+     * Variable referring to the game state.
+     */
     private String gameState = "";
 
+    /**
+     * Initialise a new game world panel with given corner, dimension and controller.
+     *
+     * @param cornerX The x coordinate for the corner of this panel.
+     * @param cornerY The y coordinate for the corner of this panel.
+     * @param width The width of this panel.
+     * @param height The height of this panel.
+     * @param controller The controller to control this panel.
+     *
+     * @effect Calls super constructor with the given coordinates and dimensions.
+     * @effect subscribes this panel as a listener to the controller.
+     * @effect The grid is initialised to a new grid with given coordinates and dimensions.
+     */
     public GameWorldPanel(int cornerX, int cornerY, int width, int height, ProgramController controller) {
         super(cornerX, cornerY, width, height);
         controller.subscribeListener(this);
         GUIGrid = new GUIGrid(cornerX, cornerY, width, height);
     }
 
+    /**
+     * Reset the game state.
+     *
+     * @post The game state is set to an empty string.
+     */
     public void resetGameText() {
         gameState = "";
     }
 
+    /**
+     * Paint the game state.
+     *
+     * @param g The graphics to paint this panel with.
+     *
+     * @effect Draws the background of this panel.
+     * @effect Draws the grid for this panel using the image library.
+     * @effect draws the game state.
+     */
     @Override
     public void paint(Graphics g) {
         drawBackground(g);
@@ -28,12 +66,22 @@ public class GameWorldPanel extends GamePanel implements ProgramListener {
         drawGameState(g);
     }
 
+    /**
+     * Draw the background of this panel.
+     *
+     * @param g The graphics to draw the background with.
+     */
     @Override
     protected void drawBackground(Graphics g) {
         g.drawImage(library.getGameWorldBackgroundImage(), getLeftCorner().x, getLeftCorner().y, getSize().x, getSize().y, null);
         panelRectangle.paintNonFill(g);
     }
 
+    /**
+     * Draw the gamestate of this panel.
+     *
+     * @param g The graphics to draw the game state with.
+     */
     private void drawGameState(Graphics g) {
         Font currentFont = g.getFont();
         Font newFont = currentFont.deriveFont(currentFont.getSize() * 4F);
@@ -42,26 +90,41 @@ public class GameWorldPanel extends GamePanel implements ProgramListener {
         g.setFont(currentFont);
     }
 
+    /**
+     * Event to call when the game has been won.
+     */
     @Override
     public void onGameWon() {
         gameState = "YOU WIN!  :)";
     }
 
+    /**
+     * Event to call when the game has been lost.
+     */
     @Override
     public void onGameLost() {
         gameState = "YOU LOSE!  :(";
     }
 
+    /**
+     * Event to call when the program has been reset.
+     */
     @Override
     public void onProgramReset() {
         gameState = "";
     }
 
+    /**
+     * Event to call when there are too many programs.
+     */
     @Override
     public void onTooManyPrograms() {
         gameState = "TOO MANY PROGRAMS!";
     }
 
+    /**
+     * Event to call when there is an invalid program.
+     */
     @Override
     public void onProgramInvalid() {
         gameState = "INVALID PROGRAM";
--- src/main/java/GUI/Panel/PalettePanel.java
+++ src/main/java/GUI/Panel/PalettePanel.java
@@ -2,22 +2,49 @@ package GUI.Panel;
 
 import Controllers.ProgramController;
 import GUI.Blocks.GUIBlock;
-import GUI.Components.GUIBlockHandler;
-import System.BlockStructure.Blocks.Block;
-import Utility.Position;
 
 import java.awt.*;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
 
+/**
+ * A class for the palette panel. This is the panel where the
+ * blocks can be selected from.
+ *
+ * @author Alpha-team
+ */
 public class PalettePanel extends GamePanel {
 
+    /**
+     * Variable referring to the controller of this panel.
+     */
     private ProgramController controller;
+    /**
+     * Variable referring to the block ID's in this panel.
+     */
     private final List<String> IDList;
+    /**
+     * Variables referring to the blocks in this panel.
+     */
     public List<GUIBlock> blocks = new ArrayList<>();
 
+    /**
+     * Initialise a new panel with given coordinates, dimensions and  controller.
+     *
+     * @param cornerX The x coordinate for the corner of this panel.
+     * @param cornerY The y coordinate for the corner of this panel.
+     * @param width The width of this panel.
+     * @param height The height of this panel.
+     * @param controller The controller to control this panel.
+     *
+     * @effect Super constructor is called with given coordinates and dimensions.
+     * @effect The ID list is set to all the different block ID's
+     * @effect The palette is refilled.
+     * @effect The block positions are set.
+     *
+     * @post The controller of this panel is set to the given controller.
+     */
     public PalettePanel(int cornerX, int cornerY, int width, int height, ProgramController controller) {
         super(cornerX, cornerY, width, height);
         this.controller = controller;
@@ -26,30 +53,67 @@ public class PalettePanel extends GamePanel {
         setBlockPositions();
     }
 
+    /**
+     * Get the blocks in this palette.
+     *
+     * @return A copy of the blocks in this palette.
+     */
     public List<GUIBlock> getBlocks() {
         return new ArrayList<>(blocks);
     }
 
+    /**
+     * Get a new block with the given ID and coordinates.
+     *
+     * @param ID The id for the new block.
+     * @param x The x coordinate for the new block.
+     * @param y The y coordinate for the new block.
+     *
+     * @return A new GUI block with the given id and coordinates.
+     */
     public GUIBlock getNewBlock(String ID, int x, int y) {
         return controller.getBlock(ID, x, y);
     }
 
+    /**
+     * Draw all the blocks in the panel.
+     *
+     * @param g The graphics to draw the blocks with.
+     */
     public void drawBlocks(Graphics g) {
         for (GUIBlock block : blocks) {
             block.paint(g);
         }
     }
 
+    /**
+     * Paint this palette panel.
+     *
+     * @param g The graphics to paint this panel with.
+     */
     @Override
     public void paint(Graphics g) {
         drawBackground(g);
     }
 
+    /**
+     * Draw the background of this panel.
+     *
+     * @param g The graphics to draw the background with.
+     */
+    @Override
     protected void drawBackground(Graphics g) {
         g.drawImage(library.getPaletteBackgroundImage(), getLeftCorner().x, getLeftCorner().y, getSize().x, getSize().y, null);
         panelRectangle.paintNonFill(g);
     }
 
+    /**
+     * Update this panel.
+     *
+     * @effect If the maximum number of blocks is reached, then the blocks are cleared.
+     * @effect Else f there are no blocks in the list, the palette is refilled and the blocks
+     *         are positioned properly.
+     */
     public void update() {
         if (controller.reachedMaxBlocks()) {
             blocks.clear();
@@ -59,16 +123,25 @@ public class PalettePanel extends GamePanel {
         }
     }
 
+    /**
+     * Refill this palette.
+     *
+     * @effect If the controller hasn't reached the maximum number of blocks, then
+     *         are all with in the ID list initialised.
+     */
     private void refillPalette() {
         if (!controller.reachedMaxBlocks()) {
             for (String id : IDList) {
-                if (!controller.reachedMaxBlocks()) {
-                    blocks.add(controller.getBlock(id, 0, 0));
-                }
+                blocks.add(controller.getBlock(id, 0, 0));
             }
         }
     }
 
+    /**
+     * Set the positions of the blocks in the palette.
+     *
+     * @effect The blocks are set in the palette underneath each other.
+     */
     private void setBlockPositions() {
         int freeHeightPerBlock = (panelRectangle.getHeight() - getTotalBlockHeight()) / (blocks.size() + 1);
         int currentHeight = freeHeightPerBlock;
@@ -79,14 +152,16 @@ public class PalettePanel extends GamePanel {
         }
     }
 
+    /**
+     * Get the total number of blocks height in this panel.
+     *
+     * @return The sum of the heights of all the blocks in this palette.
+     */
     private int getTotalBlockHeight() {
-
         int totalHeight = 0;
-
         for (GUIBlock block : blocks) {
             totalHeight += block.getHeight();
         }
-
         return totalHeight;
     }
 }
