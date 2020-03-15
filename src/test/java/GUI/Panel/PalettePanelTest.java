78a0e9d77cb2ae2f0ecd898778762f7706f4f730
Louis Carpentier <louis.carpentier.business@gmail.com>
2020-03-15 17:08:58 +0100

    Correct assertion package

--- src/test/java/GUI/Panel/PalettePanelTest.java
+++ src/test/java/GUI/Panel/PalettePanelTest.java
@@ -9,8 +9,7 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 class PalettePanelTest {
 
--- src/test/java/GUI/Panel/ProgramAreaPanelTest.java
+++ src/test/java/GUI/Panel/ProgramAreaPanelTest.java
@@ -11,7 +11,7 @@ import org.junit.jupiter.api.Test;
 
 import java.awt.*;
 
-import static org.junit.Assert.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 class ProgramAreaPanelTest {
 
