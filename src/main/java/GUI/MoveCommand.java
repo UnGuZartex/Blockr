d101edd7372e4cd32755da739ded60b539689801
Jelle Van De Sijpe <jelle.vandesijpe@gmail.com>
2020-04-23 16:42:41 +0200

    comments

--- src/main/java/GUI/MoveCommand.java
+++ src/main/java/GUI/MoveCommand.java
@@ -20,21 +20,25 @@ public class MoveCommand implements Command
     @Override
     public void execute() {
         this.snapshot = guiBlockHandler.createSnapshot();
-        System.out.println("created snapshot");
+        System.out.println("created snapshot " + this.snapshot);
         guiBlockHandler.handleMouseEvent(id, x, y);
     }
 
     @Override
     public void undo() {
         Snapshot snapshot = guiBlockHandler.createSnapshot();
+        System.out.println("created snapshot " + snapshot);
         guiBlockHandler.loadSnapshot(this.snapshot);
+        System.out.println("loaded undo snapshot " + this.snapshot);
         this.snapshot = snapshot;
     }
 
     @Override
     public void redo() {
         Snapshot snapshot = guiBlockHandler.createSnapshot();
+        System.out.println("created snapshot " + snapshot);
         guiBlockHandler.loadSnapshot(this.snapshot);
+        System.out.println("loaded redo snapshot " + this.snapshot);
         this.snapshot = snapshot;
     }
 }
