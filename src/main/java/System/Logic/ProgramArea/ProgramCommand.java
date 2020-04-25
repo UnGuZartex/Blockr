081dd19bf62a1b0673f70d6d473b6c1c0e995ed8
Louis Carpentier <louis.carpentier.business@gmail.com>
2020-04-25 17:26:43 +0200

    Doc in program commands

--- src/main/java/System/Logic/ProgramArea/ProgramCommand.java
+++ src/main/java/System/Logic/ProgramArea/ProgramCommand.java
@@ -3,16 +3,48 @@ package System.Logic.ProgramArea;
 import GameWorldAPI.History.Snapshot;
 import Utility.Command;
 
+/**
+ * A class for program commands.
+ *
+ * @author Alpha-team
+ */
 public abstract class ProgramCommand implements Command {
 
+    /**
+     * Variable referring to the program area this program command executes on.
+     */
     protected final ProgramArea programArea;
+    /**
+     * Variable referring to the snapshot of the program to keep track of.
+     */
     private Snapshot programSnapShot;
+    /**
+     * Variable referring to the snapshot of the game world to keep track of.
+     */
     private Snapshot gameWorldSnapshot;
 
+    /**
+     * Initialse a new program command with given program area.
+     *
+     * @param programArea The program are for this program command.
+     *
+     * @post The program area of this program command is set to the given
+     *       program area.
+     */
     protected ProgramCommand(ProgramArea programArea) {
         this.programArea = programArea;
     }
 
+    /**
+     * Execute this program command.
+     *
+     * @effect Create a snapshot the program in the program area.
+     * @effect Create a snapshot the game world in the program area.
+     * @effect Execute the task of this program area.
+     *
+     * @throws IllegalStateException
+     *         When the program area doesn't have 1 valid program.
+     */
     @Override
     public void execute() throws IllegalStateException {
         if (programArea.getProgram() == null || !programArea.getProgram().isValidProgram()) {
@@ -23,6 +55,13 @@ public abstract class ProgramCommand implements Command {
         executeTask();
     }
 
+    /**
+     * Undo this program command.
+     *
+     * @effect Load the snapshot of the program in the program of the program area.
+     * @effect Load the snapshot of the game world in the game world of the program area.
+     * @effect Notify the porgram area that the program state has changed.
+     */
     @Override
     public void undo() {
         programArea.getProgram().loadSnapshot(programSnapShot);
@@ -30,5 +69,8 @@ public abstract class ProgramCommand implements Command {
         programArea.notifyProgramState();
     }
 
+    /**
+     * Execute the task of this program command.
+     */
     protected abstract void executeTask();
 }
--- src/main/java/System/Logic/ProgramArea/RunProgramCommand.java
+++ src/main/java/System/Logic/ProgramArea/RunProgramCommand.java
@@ -1,11 +1,28 @@
 package System.Logic.ProgramArea;
 
+/**
+ * A class for program commands to run a program step.
+ *
+ * @author Alpha-team
+ */
 public class RunProgramCommand extends ProgramCommand {
 
+    /**
+     * Initialise a new run program command with given program area.
+     *
+     * @param programArea The program area for this run program command.
+     *
+     * @effect Calls the super constructor with given program area.
+     */
     public RunProgramCommand(ProgramArea programArea) {
         super(programArea);
     }
 
+    /**
+     * Execute the task of this run program command.
+     *
+     * @effect A program step is run in the program area of this program command.
+     */
     @Override
     protected void executeTask() {
         programArea.runProgramStep();
