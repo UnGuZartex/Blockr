31d890404a6e3c1ab5ce64a6a45656bb446d046c
Jelle Van De Sijpe <jelle.vandesijpe@gmail.com>
2020-04-26 14:27:07 +0200

    added comments

--- src/main/java/GUI/Components/MoveBlockCommand.java
+++ src/main/java/GUI/Components/MoveBlockCommand.java
@@ -6,18 +6,56 @@ import Utility.Position;
 
 import java.awt.event.MouseEvent;
 
+/**
+ * A class representing a move block command.
+ *
+ * @author Alpha-team
+ */
 public class MoveBlockCommand implements Command
 {
+    /**
+     * Variable referring to the start and end position of the mouse given to this command.
+     */
     private final Position start, end;
+
+    /**
+     * Variable referring to the linked gui block handler.
+     */
     private final GUIBlockHandler guiBlockHandler;
+
+    /**
+     * Variable referring to the stored gui block snapshot.
+     */
     private Snapshot GUIBlocksSnapshot;
 
+    /**
+     * Create a new move block command with a given start position, end position
+     * and gui block handler.
+     *
+     * @param start The given start position.
+     * @param end The given end position.
+     * @param guiBlockHandler The given gui block handler.
+     *
+     * @post The current start position is set to the given start position.
+     * @post The current end position is set to the given end position.
+     * @post The current gui block handler is set to the given gui block handler.
+     */
     public MoveBlockCommand(Position start, Position end, GUIBlockHandler guiBlockHandler) {
         this.start = start;
         this.end = end;
         this.guiBlockHandler = guiBlockHandler;
     }
 
+    /**
+     * execute this move block command.
+     *
+     * @post The current gui block handler snapshot is set to
+     * a newly created snapshot.
+     *
+     * @effect Pressing the mouse is simulated at the start position.
+     * @effect Dragging the mouse is simulated at the end position.
+     * @effect Releasing the mouse is simulated at the end position.
+     */
     @Override
     public void execute() {
         GUIBlocksSnapshot = guiBlockHandler.createSnapshot();
@@ -26,6 +64,12 @@ public class MoveBlockCommand implements Command
         guiBlockHandler.handleMouseEvent(MouseEvent.MOUSE_RELEASED, end.getX(), end.getY());
     }
 
+    /**
+     * Undo this move block command.
+     *
+     * @Effect The currently stored gui block handler snapshot is loaded
+     *         into the current gui block handler.
+     */
     @Override
     public void undo() {
         guiBlockHandler.loadSnapshot(GUIBlocksSnapshot);
