52e754f4b7bb3d0841871382858be7028b3a3fa7
Louis Carpentier <louis.carpentier.business@gmail.com>
2020-05-14 17:35:34 +0200

    ProcedureCall Test

--- src/main/java/System/BlockStructure/Blocks/ProcedureCall.java
+++ src/main/java/System/BlockStructure/Blocks/ProcedureCall.java
@@ -6,44 +6,133 @@ import System.BlockStructure.Functionality.DummyFunctionality;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * A class for procedure calls which can call a procedure.
+ *
+ * @invar Each procedure call must have a valid procedure.
+ *        | isValidProcedure(procedure)
+ *
+ * @author Alpha-team
+ */
 public class ProcedureCall extends FunctionalBlock implements BlockListener {
 
+    /**
+     * Variable referring to the procedure this call refers to.
+     */
     private final ProcedureBlock procedure;
-
+    /**
+     * Variable referring to all the listeners of this block.
+     */
     private final List<BlockListener> listeners = new ArrayList<>();
 
-    public ProcedureCall(ProcedureBlock procedure) {
+    /**
+     * Initialise a new procedure call.
+     *
+     * @param procedure The procedure for this call.
+     *
+     * @effect The super constructor is called with a dummy functionality.
+     *
+     * @post The procedure is set to the given procedure if it is valid.
+     *
+     * @throws IllegalArgumentException
+     *         If the given procedure is not valid.
+     */
+    public ProcedureCall(ProcedureBlock procedure) throws IllegalArgumentException {
         super(new DummyFunctionality());
+        if (!isValidProcedure(procedure)) {
+            throw new IllegalArgumentException("The given procedure is invalid!");
+        }
         this.procedure = procedure;
     }
 
+    /**
+     * Check whether or not the given procedure is valid.
+     *
+     * @param procedure The procedure to check.
+     *
+     * @return True if and only if the given procedure is not null.
+     */
+    public static boolean isValidProcedure(ProcedureBlock procedure) {
+        return procedure != null;
+    }
+
+    /**
+     * Get the procedure of this procedure call.
+     *
+     * @return The procedure of this procedure call.
+     */
+    public ProcedureBlock getProcedure() {
+        return procedure;
+    }
+
+    /**
+     * Subscribe the given listener as a listener of this block.
+     *
+     * @param listener The listener to subscribe.
+     */
+    public void subscribe(BlockListener listener) {
+        listeners.add(listener);
+    }
+
+    /**
+     * Unsibscribe the given listener as a listener of this block.
+     *
+     * @param listener The listener to unsubscribe.
+     */
+    public void unSubscribe(BlockListener listener) {
+        listeners.remove(listener);
+    }
+
+    /**
+     * Terminate this block.
+     *
+     * @effect Terminate this block.
+     * @effect Notify that the procedure has been deleted.
+     */
     @Override
     public void terminate() {
         super.terminate();
         notifyProcedureDeleted();
     }
 
-    private void notifyProcedureDeleted() {
-        for (BlockListener listener : new ArrayList<>(listeners)) {
-            listener.onEvent("ProcedureDel");
-        }
-    }
-
+    /**
+     * Checks whether or not this block has valid connections.
+     *
+     * @return If this block has no next, then true is returned. Otherwise is checked whether or not
+     *         the procedure has proper connections and the block on the bottom of this call is proper
+     *         (meaning either such block doesn't exist or has proper connections).
+     */
     @Override
     public boolean hasProperConnections() {
         return !hasNext() || (procedure.hasProperConnections() && (!getSubConnectorAt(0).isConnected() || getSubConnectorAt(0).getConnectedBlock().hasProperConnections()));
     }
 
+    /**
+     * Checks whether or not this block has a next block
+     *
+     * @return True if and only if the given procedure is not terminated and not passed.
+     */
     @Override
     public boolean hasNext() {
-        if (procedure != null) {
-            return !procedure.isTerminated() && !procedure.isPassed();
-        }
-        return false;
+        return !procedure.isTerminated() && !procedure.isPassed();
     }
 
+    /**
+     * Get the next block of this block.
+     *
+     * @return If this block has a next block, then the procedure is returned, otherwise the
+     *         return to block is returned.
+     *
+     * @effect If this block has a next block, then the next return to block is set.
+     *
+     * @throws IllegalStateException
+     *         When this block is terminated
+     */
     @Override
-    public Block getNext() {
+    public Block getNext() throws IllegalStateException {
+        if (isTerminated()) {
+            throw new IllegalStateException("This block is terminated!");
+        }
         if (hasNext()) {
             setReturnToOfNext();
             return procedure;
@@ -51,22 +140,21 @@ public class ProcedureCall extends FunctionalBlock implements BlockListener {
         return getReturnToBlock();
     }
 
-    private void setReturnToOfNext() {
-        if (getSubConnectorAt(0).isConnected()) {
-            procedure.setReturnToBlock(getSubConnectorAt(0).getConnectedBlock());
-            getSubConnectorAt(0).getConnectedBlock().setReturnToBlock(getReturnToBlock());
-        } else {
-            if (getReturnToBlock() != procedure.getReturnToBlock()) {
-                procedure.setReturnToBlock(getReturnToBlock());
-            }
-        }
-    }
-
+    /**
+     * Get the block at the given index.
+     *
+     * @param index The index of the block to get.
+     *
+     * @return The block at the given index, if this block has a next block is in
+     *         the procedure searched, otherwise is the super index used.
+     *
+     * @throws IllegalArgumentException
+     *         When the given index is smaller than 0.
+     */
     @Override
-    public Block getBlockAtIndex(int index) {
-
+    public Block getBlockAtIndex(int index) throws IllegalArgumentException {
         if (index < 0) {
-            return null;
+            throw new IllegalArgumentException("The index can't be smaller than 0!");
         }
         if (index == 0) {
             return this;
@@ -81,11 +169,21 @@ public class ProcedureCall extends FunctionalBlock implements BlockListener {
         return super.getBlockAtIndex(index);
     }
 
+    /**
+     * Get the index of the given block.
+     *
+     * @param block The block to get the index of.
+     *
+     * @return If this block has a next block, then is the default index used,
+     *         otherwise is the super block searched.
+     *
+     * @throws IllegalArgumentException
+     *         When the given block is null.
+     */
     @Override
-    public int getIndexOfBlock(Block block) {
-
+    public int getIndexOfBlock(Block block) throws IllegalArgumentException {
         if (block == null) {
-            return -1;
+            throw new IllegalArgumentException("The given block can't be null!");
         }
         if (block == this) {
             return 0;
@@ -107,11 +205,45 @@ public class ProcedureCall extends FunctionalBlock implements BlockListener {
         return toReturn;
     }
 
-    public ProcedureBlock getProcedure() {
-        return procedure;
+    /**
+     * Executes the corresponding procedure for the given event.
+     *
+     * @param event The event name.
+     *
+     * @effect If the given event name is "ProcedureDel", then is the on procedure
+     *         deleted action executed.
+     *
+     * @throws IllegalArgumentException
+     *         If the given event does not exist.
+     */
+    @Override
+    public void onEvent(String event) throws IllegalArgumentException {
+        if (event.equals("ProcedureDel")) {
+            onProcedureDeleted();
+        } else {
+            throw new IllegalArgumentException("The given event does not exist!");
+        }
     }
 
-    public void onProcedureDeleted() {
+    /**
+     * Notify that the procedure is deleted.
+     *
+     * @effect Call the event "ProcedureDel" for all the listeners
+     */
+    private void notifyProcedureDeleted() {
+        for (BlockListener listener : new ArrayList<>(listeners)) {
+            listener.onEvent("ProcedureDel");
+        }
+    }
+
+    /**
+     * Method to call if this block is deleted.
+     *
+     * @effect Terminates this block.
+     * @effect Disconnect the mainconnector of this block (if any block is connected)
+     * @effect Disconnect the block at the first sub connector (if any block is connected)
+     */
+    private void onProcedureDeleted() {
         this.terminate();
         if (getMainConnector().isConnected()) {
             getMainConnector().disconnect();
@@ -121,18 +253,24 @@ public class ProcedureCall extends FunctionalBlock implements BlockListener {
         }
     }
 
-    public void unSubscribe(BlockListener listener) {
-        listeners.remove(listener);
-    }
-
-    public void subscribe(BlockListener listener) {
-        listeners.add(listener);
-    }
-
-    @Override
-    public void onEvent(String Event) {
-        if (Event.equals("ProcedureDel")) {
-            onProcedureDeleted();
+    /**
+     * Set the return to block of the next block
+     *
+     * @effect This call is connected throught the subconnector, then is the return to block of
+     *         the procedure set to the connected block and the return to block of the connected
+     *         block set to the return to block of this call.
+     * @effect If this block is not connected to a block on the bottom and the procedure has a
+     *         different return to block then this call, then is the return to block of the procedure
+     *         set to the return to block of this call.
+     */
+    private void setReturnToOfNext() {
+        if (getSubConnectorAt(0).isConnected()) {
+            procedure.setReturnToBlock(getSubConnectorAt(0).getConnectedBlock());
+            getSubConnectorAt(0).getConnectedBlock().setReturnToBlock(getReturnToBlock());
+        } else {
+            if (getReturnToBlock() != procedure.getReturnToBlock()) {
+                procedure.setReturnToBlock(getReturnToBlock());
+            }
         }
     }
 }
---
+++ src/test/java/System/BlockStructure/Blocks/ProcedureCallTest.java
@@ -0,0 +1,266 @@
+package System.BlockStructure.Blocks;
+
+import System.BlockStructure.Functionality.DummyFunctionality;
+import System.Logic.ProgramArea.ConnectionHandler;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProcedureCallTest {
+
+    ProcedureBlock procedure, procedureTop, procedureBottom;
+    ProcedureCall call, callTop, callBottom, callClone;
+    Block blockBottom, blockTop;
+    ConnectionHandler handler;
+
+    @BeforeEach
+    void setUp() {
+        procedure = new ProcedureBlock();
+        procedureTop = new ProcedureBlock();
+        procedureBottom = new ProcedureBlock();
+
+        call = new ProcedureCall(procedure);
+        callTop = new ProcedureCall(procedureTop);
+        callBottom = new ProcedureCall(procedureBottom);
+        callClone = (ProcedureCall)call.clone();
+
+        handler = new ConnectionHandler();
+        blockBottom = new IfBlock();
+        blockTop = new WhileBlock();
+
+        handler.connect(blockBottom, callBottom.getSubConnectorAt(0));
+        handler.connect(callTop, blockTop.getSubConnectorAt(0));
+    }
+
+    @AfterEach
+    void tearDown() {
+        procedure = null;
+        procedureTop = null;
+        procedureBottom = null;
+        call = null;
+        callTop = null;
+        callBottom = null;
+        callClone = null;
+        handler = null;
+        blockBottom = null;
+        blockTop = null;
+    }
+
+    @Test
+    void procedureBlock_invalidProcedure() {
+        assertThrows(IllegalArgumentException.class, () -> new ProcedureCall(null));
+    }
+
+    @Test
+    void isValidProcedure() {
+        assertTrue(ProcedureCall.isValidProcedure(procedure));
+        assertTrue(ProcedureCall.isValidProcedure(procedureTop));
+        assertTrue(ProcedureCall.isValidProcedure(procedureBottom));
+        assertFalse(ProcedureCall.isValidProcedure(null));
+    }
+
+    @Test
+    void getProcedure() {
+        assertEquals(procedure, call.getProcedure());
+        assertEquals(procedure, callClone.getProcedure());
+        assertEquals(procedureTop, callTop.getProcedure());
+        assertEquals(procedureBottom, callBottom.getProcedure());
+    }
+
+    @Test
+    void unSubscribe() {
+        call.unSubscribe(callClone);
+        call.terminate();
+        assertTrue(call.isTerminated());
+        assertFalse(callClone.isTerminated());
+    }
+
+    @Test
+    void terminate_withListener() {
+        call.terminate();
+        assertTrue(call.isTerminated());
+        assertTrue(callClone.isTerminated());
+    }
+
+    @Test
+    void terminate_connections() {
+        assertTrue(blockTop.getSubConnectorAt(0).isConnected());
+        assertTrue(blockBottom.isConnectedOnMain());
+        assertFalse(call.isTerminated());
+        assertFalse(callTop.isTerminated());
+        assertFalse(callBottom.isTerminated());
+
+        call.subscribe(callTop);
+        call.subscribe(callBottom);
+        call.terminate();
+
+        assertTrue(call.isTerminated());
+        assertTrue(callTop.isTerminated());
+        assertTrue(callBottom.isTerminated());
+        assertFalse(blockTop.getSubConnectorAt(0).isConnected());
+        assertFalse(blockBottom.isConnectedOnMain());
+    }
+
+    @Test
+    void hasProperConnections_hasNoNext() {
+        procedure.terminate();
+        assertFalse(call.hasNext());
+        assertTrue(call.hasProperConnections());
+    }
+
+    @Test
+    void hasProperConnections_procedureHasNoProperConnections() {
+        handler.connect(new IfBlock(), procedure.getSubConnectorAt(0));
+        assertFalse(procedure.hasProperConnections());
+        assertFalse(call.hasProperConnections());
+    }
+
+    @Test
+    void hasProperConnections_notConnectedOnBottom() {
+        assertFalse(callTop.getSubConnectorAt(0).isConnected());
+        assertTrue(procedureTop.hasProperConnections());
+        assertTrue(callTop.hasProperConnections());
+    }
+
+    @Test
+    void hasProperConnections_invalidOnBottom() {
+        assertTrue(callBottom.getSubConnectorAt(0).isConnected());
+        assertTrue(procedureBottom.hasProperConnections());
+        assertFalse(blockBottom.hasProperConnections());
+        assertFalse(callBottom.hasProperConnections());
+    }
+
+    @Test
+    void hasProperConnections_validOnBottom() {
+        handler.connect(new FunctionalBlock(new DummyFunctionality()), call.getSubConnectorAt(0));
+        assertTrue(call.getSubConnectorAt(0).isConnected());
+        assertTrue(procedure.hasProperConnections());
+        assertTrue(call.getSubConnectorAt(0).getConnectedBlock().hasProperConnections());
+        assertTrue(call.hasProperConnections());
+    }
+
+    @Test
+    void hasNext_noneTerminatedProcedure() {
+        assertFalse(procedure.isTerminated());
+        assertTrue(call.hasNext());
+    }
+
+    @Test
+    void hasNext_terminatedProcedure() {
+        procedure.terminate();
+        assertTrue(procedure.isTerminated());
+        assertFalse(call.hasNext());
+    }
+
+    @Test
+    void getNext_terminated() {
+        call.terminate();
+        assertTrue(call.isTerminated());
+        assertThrows(IllegalStateException.class, () -> call.getNext());
+    }
+
+    @Test
+    void getNext_hasNoNext() {
+        procedureBottom.terminate();
+        assertFalse(callBottom.hasNext());
+        assertEquals(procedure.getReturnToBlock(), callBottom.getNext());
+    }
+
+    @Test
+    void getNext() {
+        assertEquals(procedure, call.getNext());
+        assertEquals(call.getReturnToBlock(), procedure.getReturnToBlock());
+
+        assertEquals(procedureBottom, callBottom.getNext());
+        assertEquals(blockBottom, procedureBottom.getReturnToBlock());
+        assertEquals(callBottom.getReturnToBlock(), blockBottom.getReturnToBlock());
+
+        Block clone = callTop.clone();
+        handler.connect(new FunctionalBlock(new DummyFunctionality()), clone.getSubConnectorAt(0));
+        assertEquals(procedureTop, clone.getNext());
+        assertEquals(clone.getSubConnectorAt(0).getConnectedBlock(), procedureTop.getReturnToBlock());
+
+        assertFalse(callTop.getSubConnectorAt(0).isConnected());
+        assertNotEquals(callTop.getReturnToBlock(), procedureTop.getReturnToBlock());
+
+        assertEquals(procedureTop, callTop.getNext());
+        assertEquals(callTop.getReturnToBlock(), procedureTop.getReturnToBlock());
+    }
+
+    @Test
+    void getBlockAtIndex_illegalIndex() {
+        assertThrows(IllegalArgumentException.class, () -> call.getBlockAtIndex(-1));
+    }
+
+    @Test
+    void getBlockAtIndex_noNext() {
+        procedureBottom.terminate();
+        assertFalse(callBottom.hasNext());
+        assertEquals(callBottom, callBottom.getBlockAtIndex(0));
+        assertEquals(blockBottom, callBottom.getBlockAtIndex(1));
+    }
+
+    @Test
+    void getBlockAtIndex() {
+        assertEquals(call, call.getBlockAtIndex(0));
+        assertEquals(procedure, call.getBlockAtIndex(1));
+
+        assertEquals(callBottom, callBottom.getBlockAtIndex(0));
+        assertEquals(procedureBottom, callBottom.getBlockAtIndex(1));
+        assertEquals(blockBottom, callBottom.getBlockAtIndex(2));
+    }
+
+    @Test
+    void getIndexOfBlock_illegalBlock() {
+        assertThrows(IllegalArgumentException.class, () -> call.getIndexOfBlock(null));
+    }
+
+    @Test
+    void getIndexOfBlock_noNext() {
+        procedureBottom.terminate();
+        assertFalse(callBottom.hasNext());
+        assertEquals(0, callBottom.getIndexOfBlock(callBottom));
+        assertEquals(1, callBottom.getIndexOfBlock(blockBottom));
+    }
+
+    @Test
+    void getIndexOfBlock() {
+        assertEquals(0, call.getIndexOfBlock(call));
+        assertEquals(1, call.getIndexOfBlock(procedure));
+
+        assertEquals(0, callBottom.getIndexOfBlock(callBottom));
+        assertEquals(1, callBottom.getIndexOfBlock(procedureBottom));
+        assertEquals(2, callBottom.getIndexOfBlock(blockBottom));
+    }
+
+    @Test
+    void testClone() {
+        Block clone = call.clone();
+        assertNotEquals(clone, call);
+        assertEquals(((ProcedureCall)clone).getProcedure(), call.getProcedure());
+
+        call.terminate();
+        assertTrue(call.isTerminated());
+        assertTrue(clone.isTerminated());
+    }
+
+    @Test
+    void onEvent_invalidEvent() {
+        assertThrows(IllegalArgumentException.class, () -> call.onEvent("InvalidEventName"));
+    }
+
+    @Test
+    void onEvent() {
+        assertFalse(call.isTerminated());
+        assertFalse(callClone.isTerminated());
+        call.onEvent("ProcedureDel");
+        assertTrue(call.isTerminated());
+        assertTrue(callClone.isTerminated());
+
+        assertTrue(callTop.isConnectedOnMain());
+        callTop.onEvent("ProcedureDel");
+        assertFalse(callTop.isConnectedOnMain());
+    }
+}
